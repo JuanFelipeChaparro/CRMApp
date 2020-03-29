dab888c5829e9859b45f854ed06da61e9bb40e9b
Juan Chaparro <juanch@outlook.com>
2020-03-29 01:09:46 -0500

    New Customer - Completed
    Completed with emails

--- client/src/componentes/NuevoCliente.js
+++ client/src/componentes/NuevoCliente.js
@@ -9,10 +9,40 @@ class NuevoCliente extends Component {
             apellido: '',
             empresa: '',
             edad: '',
-            email: '',
+            emails: [],
             tipo: ''
         },
         error: false
+    }
+
+    nuevoCampo = () => {
+        this.setState({
+            cliente: {
+                ...this.state.cliente,
+                emails: this.state.cliente.emails.concat([{email: ''}])
+            }
+        });
+    }
+
+    quitarCampo = (index) => {
+        this.setState({
+            cliente: {
+                ...this.state.cliente,
+                emails: this.state.cliente.emails.filter((email, i) => index !== i)
+            }
+        });
+    }
+
+    leerCampo = (evt, index) => {
+        const { emails } = this.state.cliente;
+        emails[index] = {email: evt.target.value};
+
+        this.setState({
+            cliente: {
+                ...this.state.cliente,
+                emails,
+            }
+        });
     }
 
     render() {
@@ -30,12 +60,10 @@ class NuevoCliente extends Component {
                                 onSubmit={evt => {
                                     evt.preventDefault();
                                     const input = this.state.cliente;
-                                    
+
                                     if (input.nombre === '' || input.apellido === '' || input.empresa === '' || input.tipo === '') {
                                         this.setState({error: true});
                                         return;
-                                    } else {
-                                        this.setState({error: false});
                                     }
                                    
                                     crearCliente({
@@ -76,7 +104,7 @@ class NuevoCliente extends Component {
                                     </div>
                                 </div>
                                 <div className="form-row">
-                                    <div className="form-group col-md-6">
+                                    <div className="form-group col-md-12">
                                         <label>Empresa</label>
                                         <input 
                                             type="text" 
@@ -91,20 +119,24 @@ class NuevoCliente extends Component {
                                                 })
                                             }}/>
                                     </div>
-                                    <div className="form-group col-md-6">
-                                        <label>Email</label>
-                                        <input 
-                                            type="email" 
-                                            className="form-control" 
-                                            placeholder="Email"
-                                            onChange={evt => {
-                                                this.setState({
-                                                    cliente: {
-                                                        ...this.state.cliente,
-                                                        email: evt.target.value
-                                                    }
-                                                })
-                                            }}/>
+                                    {this.state.cliente.emails.map((input, index) => (
+                                        <div key={index} className="form-group col-md-12">
+                                            <label>Correo {index + 1}:</label>
+                                            <div className="input-group">
+                                                <input
+                                                    value={input.email}
+                                                    type="email"
+                                                    placeholder="Email"
+                                                    className="form-control" 
+                                                    onChange={evt => this.leerCampo(evt, index)}/>
+                                                <div className="input-group-append">
+                                                    <button type="button" className="btn btn-danger" onClick={() => this.quitarCampo(index)}>&times; Eliminar</button>
+                                                </div>
+                                            </div>
+                                        </div>   
+                                    ))}
+                                    <div className="form-group d-flex justify-content-center col-md-12">
+                                        <button className="btn btn-warning" type="button" onClick={this.nuevoCampo}>+ Agregar Email</button>
                                     </div>
                                 </div>
                                 <div className="form-row">
